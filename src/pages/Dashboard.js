0d0415d813b4d97914e9157b4ddc270ac98ec7d8
khaled sidi <dah_khaled@yahoo.fr>
2022-10-01 20:38:30 +0300

    second commit

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -5,22 +5,19 @@ import { GithubContext, useGlobalContext } from "../context/context";
 const Dashboard = () => {
   const { loading } = useGlobalContext();
 
-  if (loading) {
-    return (
-      <main>
-        <Navbar></Navbar>
-        <Search></Search>
-        <img src={spinnervlll} className="loading-img" alt="spinnervlll" />
-      </main>
-    );
-  }
+  // if (loading) {
+  // return (
+  //   <main>
+  //     <Navbar></Navbar>
+  //     <Search></Search>
+  //     <img src={spinnervlll} className="loading-img" alt="spinnervlll" />
+  //   </main>
+  // );
+  // }
   return (
     <main>
-      <Navbar></Navbar>
-      <Search></Search>
-      <Info></Info>
-      <User></User>
-      <Repos></Repos>
+      <Navbar> </Navbar> <Search> </Search> <Info> </Info> <User> </User>{" "}
+      <Repos> </Repos>
     </main>
   );
 };
